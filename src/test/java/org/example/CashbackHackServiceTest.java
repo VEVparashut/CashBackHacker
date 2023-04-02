82f0a894cc40ba9e378078c4948973532a8486ab
Neto test <89373786148@yandex.ru>
2023-04-02 18:03:06 +0300

    first commit testng

---
+++ src/test/java/org/example/CashbackHackServiceTest.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void test1() {
+
+        int amount = 650;
+
+        int expected = 350;
+        int actual = service.remain(amount);
+
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    public void testMin() {
+
+        int amount = 0;
+
+        int expected = 1000;
+        int actual = service.remain(amount);
+
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    public void testMax() {
+
+        int amount = 999;
+
+        int expected = 1;
+        int actual = service.remain(amount);
+
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    public void testThousand() {
+
+        int amount = 1000;
+
+        int expected = 0;
+        int actual = service.remain(amount);
+
+        assertEquals(actual,expected);
+    }
+}
